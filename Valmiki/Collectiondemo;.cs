95ab3a0d3ebf2e377cba20cc44a08a50a8177300
rev2thi14 <revathivalmiki14@gmail.com>
2021-12-30 11:20:54 +0530

    Read a line

---
+++ Valmiki/Collectiondemo;.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace CollectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           List<string> mylist = new List<string>();
+            mylist.Add("abc");
+            mylist.Add("cdeaf");
+            mylist.Add("js");
+            mylist.Add("php");
+            mylist.Remove("a");
+            foreach (string i in mylist)
+            {
+             
+            }
+                Console.WriteLine();
+            }
+          
+        }
+        
+
+
+
+        }
